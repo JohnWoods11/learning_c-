d4a237eb6e9b66af9986acd898cbb2fc7dd45c35
JohnWoods11 <itwasabet@hotmail.com>
2019-01-14 21:31:15 +0000

    Removed stdlibfacilities from caculator.cpp as it was not working. Replaced with the relevant standard library functions and functions from within the stdlibfacilities.h header file. calculator.cpp now works as was initialy intended.

--- Chapter6/try-these/calculator.cpp
+++ Chapter6/try-these/calculator.cpp
@@ -4,8 +4,47 @@
 // "Software - Principles and Practice using C++" by Bjarne Stroustrup
 //
 
-#include "../stdlibfacilities.h"
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<cstdlib>
+#include<string>
+#include<list>
+#include<vector>
+#include<algorithm>
+#include<stdexcept>
 
+using namespace std;
+
+inline void keep_window_open()
+{
+	cin.clear();
+	cout << "Please enter a character to exit\n";
+	char ch;
+	cin >> ch;
+	return;
+}
+
+inline void keep_window_open(string s)
+{
+	if (s=="") return;
+	cin.clear();
+	cin.ignore(120,'\n');
+	for (;;) {
+		cout << "Please enter " << s << " to exit\n";
+		string ss;
+		while (cin >> ss && ss!=s)
+			cout << "Please enter " << s << " to exit\n";
+		return;
+    }
+}
+
+// error() simply disguises throws:
+inline void error(const string& s)
+{
+	throw runtime_error(s);
+}
 //------------------------------------------------------------------------------
 
 class Token {
@@ -75,7 +114,7 @@ double primary()     // read and evaluate a Primary
 int main()
 try {
     while (cin)
-        cout << expression() << '\n';
+        cout << "= " << expression() << '\n';
     keep_window_open("~0");
 }
 catch (exception& e) {
